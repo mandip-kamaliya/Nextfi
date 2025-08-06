051560dfec4be66146b2d6ec41d0241e99fdedce
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-06 18:15:17 +0530

    added dark mode feature

--- lib/viem.ts
+++ lib/viem.ts
@@ -1,7 +1,7 @@
 import { createPublicClient , http } from "viem";
 import { sepolia } from "viem/chains";
 
-const PublicClient = createPublicClient({
+export const PublicClient = createPublicClient({
     chain:sepolia,
     transport:http("https://divine-capable-layer.ethereum-sepolia.quiknode.pro/4a5dffce2644356e226f5415fb6a3be5e22c4526/"),
 })
